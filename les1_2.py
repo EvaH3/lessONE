60c346d5ae7d7eae14e87c22a3e10db93a2c42c9
EvaH3 <evans-evs@yandex.ru>
2024-02-24 10:09:38 +0400

    111

--- les1_2.py
+++ les1_2.py
@@ -1,3 +1,51 @@
-count_1 = int(input())
-count_2 = int(input())
-print(count_1+count_2)
+print("Задание №2")
+#count_1 = int(input())
+#count_2 = int(input())
+#print(count_1+count_2)
+
+print("Задание №3")
+x=36/4*2
+print(x)
+
+print("Задание №4")
+print(4*3**2*2)
+
+print("Задание №5")
+s="tomato"
+t="cucumber"
+print(2*s+t)
+
+print("Задание №6")
+s="change me"
+print(s.replace("e","E",2))
+
+print("Задание №7")
+str_1 = "red"
+str_2 = "white"
+str_3 = "green"
+print(str_1 + str_2)
+print("_".join([str_1, str_2]))
+print("_".join([str_1, str_2]))
+print(str_3.find("a"))
+print(str_2.find("e"))
+print(str_3.find("r"))
+
+print("Задание №8")
+#N=10
+#mass = [0]*N
+#for i in range(N):
+#    mass[i] = int(input())
+#    i+=1
+#print(max(mass))
+
+print("Задание №9")
+x = [2,4,6,8,10,12]
+print(x[-1:2:-2])
+print(x[::-2])
+print(x[0::0])
+print(x[1::3])
+print(x[0])
+
+print("Задание №10")
+a=(2,"1",1,10,1)
+print(a.index(1))
